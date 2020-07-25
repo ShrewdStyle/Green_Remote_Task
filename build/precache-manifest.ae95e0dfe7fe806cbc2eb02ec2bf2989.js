c88c7426507ef4ca21437d19d23b4a041e8245ce
ShrewdStylegit config --global user.name ShrewdStylegit config --global user.name ShrewdStyle <jake.webtech@gmail.com>
2020-07-25 16:25:30 +0100

    Build folder created for deployment

---
+++ build/precache-manifest.ae95e0dfe7fe806cbc2eb02ec2bf2989.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e3e147b3cea7b2c7911e4fbd54cde6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f72f00f579448d3df2f3",
+    "url": "/static/css/main.0131b7c6.chunk.css"
+  },
+  {
+    "revision": "0e22c1162d1fa36bd564",
+    "url": "/static/js/2.39a9517a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.39a9517a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72f00f579448d3df2f3",
+    "url": "/static/js/main.5f86c50e.chunk.js"
+  },
+  {
+    "revision": "d22f7f95995f9263c686",
+    "url": "/static/js/runtime-main.d66e8c39.js"
+  },
+  {
+    "revision": "9c35bf87f23c8cca614720126fe0baa0",
+    "url": "/static/media/Gotham-Bold.9c35bf87.otf"
+  },
+  {
+    "revision": "f76e3adf545b3299f643fd7642800351",
+    "url": "/static/media/Gotham-Light.f76e3adf.otf"
+  },
+  {
+    "revision": "9da97edd306f798b1cb2eb6dca334e22",
+    "url": "/static/media/logo-r.9da97edd.svg"
+  }
+]);
