af45f8724957deeaefe2958b4f0158111ebafc82
ShrewdStylegit config --global user.name ShrewdStylegit config --global user.name ShrewdStyle <jake.webtech@gmail.com>
2020-07-25 15:24:06 +0100

    Login form now completed

--- src/components/Layout/Login/LoginForm.js
+++ src/components/Layout/Login/LoginForm.js
@@ -9,12 +9,15 @@ const LoginForm = () => {
           <img className="logoForm" src={Logo} alt="Green" />
           <h1 className="formHeader">Example login screen</h1>
           <div className="formSubHeader">Getting started with Green</div>
-          <div className="emailInput">Email Address</div>
-          <input type="email" />
+          <div className="emailHeader">Email Address</div>
+          <input className="emailInput" type="email" />
           <div className="checkBox">
-            <input type="checkbox" name="remember" />
-            <label for="remember">Remember this device</label>
+            <input className="formCheckBox" type="checkbox" name="remember" />
+            <label className="rememberText" for="remember">
+              Remember this device
+            </label>
           </div>
+          <button className="btnSignin">Sign In</button>
         </form>
       </div>
     </div>
