6e7e0126079310ed354fc7d47075eb67ebd37fcb
ShrewdStylegit config --global user.name ShrewdStylegit config --global user.name ShrewdStyle <jake.webtech@gmail.com>
2020-07-25 12:46:10 +0100

    Initial Commit - page layout and basic structure

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Home = () => {
+  return (
+    <Fragment>
+      <h1 className="testHeader">Green Remote Task</h1>
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ src/components/Pages/Login.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+const Login = () => {
+  return (
+    <Fragment>
+      <div className="test">Please login below</div>
+    </Fragment>
+  );
+};
+
+export default Login;
